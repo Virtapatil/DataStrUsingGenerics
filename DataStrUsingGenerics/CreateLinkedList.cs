cde0f669e91cd00ecf805def40dcc76291b90cbb
Virtapatil2000 <virtapatil01@gmail.com>
2023-05-23 01:34:15 +0530

    Creating linked list

---
+++ DataStrUsingGenerics/CreateLinkedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrUsingGenerics
+{
+    public class CreateLinkedList
+    {
+        public int Data;
+        public Node Next;
+
+        public CreateLinkedList(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public class CreateLinkedList
+    {
+        public Node Head;
+
+        public CreateLinkedList()
+        {
+            Head = null;
+        }
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                newNode.Next = Head;
+                Head = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+    
+}
--- DataStrUsingGenerics/Program.cs
+++ DataStrUsingGenerics/Program.cs
@@ -4,13 +4,24 @@
     {
         static void Main(string[] args)
         {
-            LinkedList linkedList = new LinkedList();
+            /*LinkedList linkedList = new LinkedList();
 
             linkedList.Add(56);
             linkedList.Add(30);
             linkedList.Add(70);
 
             Console.WriteLine("Linked List elements:");
+            linkedList.Display();
+
+            Console.ReadLine();*/
+
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.Add(70);
+            linkedList.Add(30);
+            linkedList.Add(56);
+
+            Console.WriteLine("Linked List sequence: ");
             linkedList.Display();
 
             Console.ReadLine();
