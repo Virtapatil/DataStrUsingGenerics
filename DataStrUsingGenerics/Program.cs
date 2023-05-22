b0037cb3913592a4d35d51522866852c96ecbbb7
Virtapatil2000 <virtapatil01@gmail.com>
2023-05-23 00:36:49 +0530

    Adding data Structure problems

---
+++ DataStrUsingGenerics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStrUsingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
